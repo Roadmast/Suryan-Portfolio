a2196bd411231e07f6f853a4ec5cff16ffb8aa7b
OmniSuryaNarayana <suryanarayana@allcognix.com>
2026-06-26 19:59:34 +0530

    fix5

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Required for static GitHub Pages hosting
-  basePath: process.env.GITHUB_PAGES === 'true' ? '/Suryan-Portfolio' : '', // Conditionally apply basePath for GitHub Pages
   images: {
     unoptimized: true, // Required because Next.js image optimization doesn't work on static hosts
   },
